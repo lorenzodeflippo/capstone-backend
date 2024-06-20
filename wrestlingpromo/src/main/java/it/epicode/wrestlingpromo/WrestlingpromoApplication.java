c78d5027ecc6d4a78d323b05cc4f90bd31ae09cc
lorenzodeflippo <defilippolorenzo@gmail.com>
2024-06-20 16:56:24 +0200

    Initial Commit

---
+++ wrestlingpromo/src/main/java/it/epicode/wrestlingpromo/WrestlingpromoApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.wrestlingpromo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrestlingpromoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrestlingpromoApplication.class, args);
+	}
+
+}
---
+++ wrestlingpromo/src/test/java/it/epicode/wrestlingpromo/WrestlingpromoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.wrestlingpromo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrestlingpromoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
